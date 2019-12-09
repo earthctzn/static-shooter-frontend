df5bf161c128c7032e9ffff82ed1b74fbe273091
earthctzn <earthctzn1@gmail.com>
2019-12-09 14:12:42 -0500

    Adding createGame method to GameAdapter class.

--- src/adapters/GameAdapter.js
+++ src/adapters/GameAdapter.js
@@ -6,4 +6,22 @@ class GameAdapter {
   getGames() {
     return fetch(this.gameUrl).then(res => res.json())
   }
+
+  createGame(lives=4, score=0){
+    const startinglives = {
+      lives: lives
+    }
+    const startingScore = {
+      score: score
+    }
+    return fetch(this.baseUrl, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },      
+      lives: JSON.stringify({startinglives}),
+      score: JSON.stringify({startingScore})
+    })
+    .then(res => res.json())
+  }
 }
--- src/components/game.js
+++ src/components/game.js
@@ -1,23 +1,24 @@
 class Game {
   constructor() { 
-
+    this.adapter = new GameAdapter()
   
   }
   
-ctx() {
-  let canvas = document.getElementById("gameSpace");
-  let ctx = canvas.getContext('2d')
-  return ctx
-}
+  ctx() {
+    let canvas = document.getElementById("gameSpace");
+    let ctx = canvas.getContext('2d')
+    return ctx
+  }
 
   
 
-  const GAME_WIDTH = 800;
-  const GAME_HEIGHT = 600;
+  // const GAME_WIDTH = 800;
+  // const GAME_HEIGHT = 600;
 
-  this.ctx.clearRect(0,0, 800, 600);
+  // clearRect(0,0, 800, 600);
 
-  let ship = new Ship(GAME_WIDTH, GAME_HEIGHT);
+  // let ship = new Ship(GAME_WIDTH, GAME_HEIGHT);
 
-  ship.draw(ctxt);
+  // ship.draw(ctxt);
+
 }
