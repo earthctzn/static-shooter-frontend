e642eab458576611c1952e0033cf572d0a050fb4
earthctzn <earthctzn1@gmail.com>
2019-12-10 15:44:51 -0500

    Adding loop cuntion to get the game loop going and also added preliminary movement for ship object.

--- src/components/game.js
+++ src/components/game.js
@@ -4,16 +4,24 @@ const ctx = canvas.getContext('2d')
 const GAME_WIDTH = canvas.width;
 const GAME_HEIGHT = canvas.height;
 // const i = document.getElementById('ship')
-ctx.clearRect(0,0, 800, 600);
+
 
 let newShip = new Ship(GAME_WIDTH, GAME_HEIGHT);
 
 newShip.draw(ctx);
 
+let lastTime = 0
+function loop(timestamp) {
+  let deltaTime = timestamp - lastTime
+  lastTime = timestamp
 
-// function Loop() {
+  ctx.clearRect(0,0, 800, 600);
+  newShip.update(deltaTime);
+  newShip.draw(ctx)
 
-// }
+  requestAnimationFrame(loop)
+}
+// loop()
 // // ctx.fillStyle = '#f00'
 // ctx.drawImage(i, 130, 130, 20, 10)
 
--- src/components/ship.js
+++ src/components/ship.js
@@ -2,6 +2,7 @@ class Ship {
   
   constructor(gameWidth, gameHeight) {
     this.img = document.getElementById('ship')
+    // this.missile = document.getElementById('goodMissile')
     this.width = this.img.width
     this.height = this.img.height
     this.position = {
@@ -14,4 +15,14 @@ class Ship {
     // ctx.clearRect(0, 0, 800, 600)
     ctx.drawImage(this.img, this.position.x, this.position.y, this.width, this.height)
   }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
+    this.position.x += 5 / deltaTime;
+  }
+
+
+  // shoot(ctx) {
+  //   ctx.drawImage(this.missile)
+  // }
 } 
