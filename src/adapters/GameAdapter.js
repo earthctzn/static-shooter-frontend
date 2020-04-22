290fba9da721c5ef870c9057fd504aeb05f6d218
earthctzn <earthctzn1@gmail.com>
2020-04-22 17:20:39 -0400

    refactors GameAdapter with async methods.

--- src/adapters/GameAdapter.js
+++ src/adapters/GameAdapter.js
@@ -3,22 +3,23 @@ class GameAdapter {
         this.gameUrl = 'http://localhost:3000/api/v1/games'
     }
 
-    getGames() {
-        return fetch(this.gameUrl).then(res => res.json())
+    async getGames() {
+        const res = await fetch(this.gameUrl)
+        return await res.json()
     }
 
-    createGame(score, playerName) {
+    async createGame(score, playerName) {
         const game = {
             score: score,
             player_name: playerName
         }
-        return fetch(this.gameUrl, {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                },
-                body: JSON.stringify(game)
-            })
-            .then(res => res.json())
+        const res = await fetch(this.gameUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(game)
+        })
+        return await res.json()
     }
 }
